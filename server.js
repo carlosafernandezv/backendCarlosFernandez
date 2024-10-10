e58d762717a67c845f4e30982a46252efca8c69e
Carlos Fernandez <krlosafernandez@gmail.com>
2024-10-10 09:45:10 -0500

    despues de Break

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const PORT = process.env.PORT
 
 const ready = ()=> console.log("server ready in port: "+PORT);
 
-server.listen(PORT,ready)
+server.listen(PORT,ready)   
+
+server.get('/',(request,response) =>{
+    response.send('Hola mundo en Express')
+})
+
+server.get('/mensaje',(request,response) =>{
+    response.send('Hola Express')
+})
 
 console.log(process.env.PORT);
